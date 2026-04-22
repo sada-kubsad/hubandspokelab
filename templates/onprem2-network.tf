914bb5cc4dd2f6ea585de5621767bc585e8434fc
Sada Kubsad <11302503+sada-kubsad@users.noreply.github.com>
2026-04-22 12:19:52 -0700

    Change VPN SKU from VpnGw2 to VpnGw2AZ

--- templates/onprem2-network.tf
+++ templates/onprem2-network.tf
@@ -64,7 +64,7 @@ resource "azurerm_virtual_network_gateway" "onprem-site-2-vpngw" {
 
   active_active = true
   enable_bgp    = true
-  sku           = "VpnGw2"
+  sku           = "VpnGw2AZ"
   generation    = "Generation2"  
 
   ip_configuration {
